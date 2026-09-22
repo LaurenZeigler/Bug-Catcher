ceb2cf0f914a28aa0c271bb7a0deb27bf651b7ab
Treefel <buttarsfk@gmail.com>
2026-09-21 21:35:57 -0600

    changed some bug flying logic, not quite fixed

--- bugc/assets/scripts/bugBehavior/bugBehavior.gd
+++ bugc/assets/scripts/bugBehavior/bugBehavior.gd
@@ -6,12 +6,16 @@ extends CharacterBody3D
 
 @onready var nav_agent : NavigationAgent3D = $NavigationAgent3D
 @onready var timer : Timer = $Timer
+@onready var skin : MeshInstance3D = $MeshInstance3D
 
-enum State {IDLE, WALKING, FLYING, FLYIDLE}
+enum State {IDLE, WALKING, FLYING}
 var state = State.IDLE
 
-var walking_duration = 2
-var idle_duration = 2
+@export var walking_duration = 2
+@export var idle_duration = 2
+
+@export var walk_chance : float = .7
+@export var fly_chance : float = .3
 
 var target_position : Vector3
 
@@ -26,17 +30,21 @@ func _on_timer_timout():
 		State.IDLE:
 			var random_target = Vector3(randf_range(-10,10), 0, randf_range(-10,10))
 			nav_agent.target_position = random_target
-			state = State.WALKING
+			var move_type = randf_range(0,1)
+			if move_type <= walk_chance:
+				state = State.WALKING
+			else:
+				state = State.FLYING
 			timer.start(idle_duration)
-		State.FLYIDLE:
-			var random_target = Vector3(randf_range(-10,10), fly_height, randf_range(-10,10))
-			nav_agent.target_position = random_target
-			state = State.FLYING
-			timer.start(idle_duration)
+		#State.FLYIDLE:
+			#var random_target = Vector3(randf_range(-10,10), fly_height, randf_range(-10,10))
+			#nav_agent.target_position = random_target
+			#state = State.FLYING
+			#timer.start(idle_duration)
 		State.WALKING:
 			state = State.IDLE
 		State.FLYING:
-			state = State.FLYIDLE
+			state = State.IDLE
 	
 func _process(delta : float) -> void:
 	if state == State.WALKING:
@@ -46,9 +54,9 @@ func _process(delta : float) -> void:
 		else:
 			move_toward_target(walk_speed)
 		
-	if state == State.FLYING:
+	elif state == State.FLYING:
 		if nav_agent.is_navigation_finished():
-			state = State.FLYIDLE
+			state = State.IDLE
 			timer.start(idle_duration)
 		else:
 			fly_toward_target(fly_speed)
@@ -69,9 +77,14 @@ func fly_toward_target(move_speed):
 	motion_mode = CharacterBody3D.MOTION_MODE_FLOATING
 	var next_position = nav_agent.target_position
 	var direction = (next_position - global_transform.origin).normalized()
+	
+	# for fly height make it affect the model itself instead of the base of it so that there is a reference to floor
+	# maybe change the collision box with it?
+	
+	
 	velocity = direction * move_speed
 	print(next_position)
-	print("moving to" + str(direction))
+	print("flying to" + str(direction))
 	move_and_slide()
 	if direction.length() > 0:
 		var target_rotation = global_transform.looking_at(next_position).basis
